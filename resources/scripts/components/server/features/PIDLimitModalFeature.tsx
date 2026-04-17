6e82f9c19d7375b059ec0c390b6a5094b83869c7
飒爽师叔 <43847794+vlssu@users.noreply.github.com>
2026-04-17 08:46:04 +0000

    修复工作流

--- resources/scripts/components/server/features/PIDLimitModalFeature.tsx
+++ resources/scripts/components/server/features/PIDLimitModalFeature.tsx
@@ -65,7 +65,8 @@ const PIDLimitModalFeature = () => {
                     <p css={tw`mt-4`}>此服务器已达到最大进程或内存限制。</p>
                     <p css={tw`mt-4`}>
                         在 wings 的配置文件 <code css={tw`font-mono bg-neutral-900`}>config.yml</code> 中增加{' '}
-                        <code css={tw`font-mono bg-neutral-900`}>container_pid_limit</code> 参数，可能有助于解决这个问题。
+                        <code css={tw`font-mono bg-neutral-900`}>container_pid_limit</code>{' '}
+                        参数，可能有助于解决这个问题。
                     </p>
                     <p css={tw`mt-4`}>
                         <b>注意：必须重新启动 Wings 才能使配置文件更改生效</b>
